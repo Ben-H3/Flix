99af282d4688d136a285825a697a62223bfaee74
Benjamin Charles Hora <bhora@fb.com>
2020-06-24 09:29:08 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by Benjamin Charles Hora on 6/24/20.
+//  Copyright © 2020 Ben Hora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flix
+//
+//  Created by Benjamin Charles Hora on 6/24/20.
+//  Copyright © 2020 Ben Hora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
