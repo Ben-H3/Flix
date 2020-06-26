08b7f069c54be1480b9984e796e1e915bf99ced7
Benjamin Charles Hora <bhora@fb.com>
2020-06-26 13:15:01 -0700

    Images on the table view now fade in when loaded

---
+++ Flix/View Controllers/WebViewController.h
@@ -0,0 +1,17 @@
+//
+//  WebViewController.h
+//  Flix
+//
+//  Created by Benjamin Charles Hora on 6/26/20.
+//  Copyright © 2020 Ben Hora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WebViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
